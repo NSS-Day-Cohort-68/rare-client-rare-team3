5e2ffac546f4d7bb44353425fe11f203b5427054
Jordan VanVeldhuisen <jordanvanveldhuisen@gmail.com>
2024-03-11 12:21:08 -0400

    added an all posts view that sorted by date and if its active

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,10 +1,11 @@
 import { Routes, Route, Outlet } from "react-router-dom"
 import { Welcome } from "../Welcome/Welcome"
+import { AllPostsList } from "../posts/AllPostsList"
 
 export const ApplicationViews = () => {
   return (
     <Routes>
-      <Route path="/" element={<Welcome />}></Route>
+      <Route path="/" element={<AllPostsList />}></Route>
     </Routes>
   )
 }
