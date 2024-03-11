5135b77aaadd17a34d50a5b9445e383c235e7f86
Stacy Griffy <sgriffy2@gmail.com>
2024-03-11 12:57:34 -0500

    Format Date

--- src/components/posts/MyPosts.js
+++ src/components/posts/MyPosts.js
@@ -4,6 +4,19 @@ import { getPostsByUserToken } from "../../services/postService";
 export const MyPosts = ({ currentUser }) => {
   const [posts, setPosts] = useState([]);
 
+    // Function to format date string to a readable format
+    const formatDate = (date) => {
+      const options = {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZoneName: "short",
+      };
+      return new Date(date).toLocaleDateString("en-US", options);
+    };
+
   useEffect(() => {
     // Check if currentUser.token is defined before making the fetch request
     if (currentUser && currentUser.token) {
@@ -24,6 +37,7 @@ export const MyPosts = ({ currentUser }) => {
               Author: {post.user.first_name} {post.user.last_name}
             </p>
             <p>Category: {post.category.label}</p>
+            <p>Date: {formatDate(post.publication_date)}</p>
           </div>
         );
       })}
