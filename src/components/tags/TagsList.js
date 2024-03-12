846b9358be84c339b0171497cc210e67f0f9fa30
Saygin Gecener <sgecener@yahoo.com>
2024-03-12 16:47:32 -0400

    first commit

---
+++ src/components/tags/TagsList.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react"
+
+export const TagsList = () => {
+    const [allTags, setAllTags] = useState([])
+
+    useEffect({
+        
+    }, [])
+
+    return (
+        <div>
+
+        </div>
+    )
+}
