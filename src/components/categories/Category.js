781a2c3798f39b9e43e04aefbe74acbfc3b0b868
Eric Heidel <ericleeheidel@yahoo.com>
2024-03-12 11:34:01 -0500

    deleteCategory() and handleDeleteCategory() self tested ready for peer testing

--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -1,14 +1,35 @@
 import { Link } from "react-router-dom"
 import "./Category.css"
+import { deleteCategory } from "../../services/categoriesService.js"
 
-export const Category = ({ category }) => {
+export const Category = ({ category, getAndSetAllCategories }) => {
+  const handleDeleteCategory = () => {
+    getAndSetAllCategories()
+
+    const confirmDelete = window.confirm(
+      `Are you sure you want to\ndelete this category?`
+    )
+
+    if (confirmDelete) {
+      deleteCategory(category.id).then(() => {
+        getAndSetAllCategories()
+      })
+    } else {
+      return null
+    }
+  }
+
   return (
-    <div className="category" value>
+    <div className="category">
       <button className="category-settings">
-        <i class="fa-solid fa-gear"></i>
+        <i className="fa-solid fa-gear"></i>
       </button>
-      <button className="category-delete">
-        <i class="fa-solid fa-trash-can"></i>
+      <button
+        className="category-delete"
+        value={category.id}
+        onClick={handleDeleteCategory}
+      >
+        <i className="fa-solid fa-trash-can"></i>
       </button>
       <Link>
         <div className="category-name-link">{category.label}</div>
