3448d94ee580964679b8640bad28367af521f385
Eric Heidel <ericleeheidel@yahoo.com>
2024-03-12 10:07:40 -0500

    deleteCategory() added

--- src/services/categoriesService.js
+++ src/services/categoriesService.js
@@ -1,3 +1,9 @@
 export const getAllCategories = () => {
   return fetch(`http://localhost:9999/categories`).then((res) => res.json())
 }
+
+export const deleteCategory = (catId) => {
+  return fetch(`http://localhost:9999/categories/${catId}`, {
+    method: "DELETE",
+  })
+}
