65316427e818a05dde228211785837cab0970f16
Jordan VanVeldhuisen <jordanvanveldhuisen@gmail.com>
2024-03-12 11:38:54 -0400

    added more to edit post form

--- src/components/forms/EditPostForm.js
+++ src/components/forms/EditPostForm.js
@@ -1,8 +1,26 @@
 import { useEffect } from "react"
 import { useNavigate, useParams } from "react-router-dom"
+import { getPostById, updatePost } from "../../services/postService"
 
 export const EditPostForm = ({ currentUser }) => {
   const [post, setPost] = useEffect({})
   const { postId } = useParams()
   const navigate = useNavigate()
+
+  useEffect(() => {
+    getPostById(postId).then((post) => {
+      setPost(post)
+    })
+  }, [postId])
+
+  const handleInputChange = (event) => {
+    const stateCopy = { ...post }
+    stateCopy[event.target.name] = event.target.value
+    setPost(stateCopy)
+  }
+
+  const handleUpdate = (event) => {
+    const changedPostObj = structuredClone(post)
+    updatePost(changedPostObj)
+  }
 }
--- src/services/postService.js
+++ src/services/postService.js
@@ -1,9 +1,24 @@
 export const getPostsByUserToken = async (userToken) => {
   return await fetch(
     `http://localhost:9999/posts?user_id=${userToken}&_expand=user`
-  ).then((res) => res.json());
-};
+  ).then((res) => res.json())
+}
 
 export const getAllPosts = () => {
   return fetch(`http://localhost:9999/posts`).then((res) => res.json())
+}
+
+export const getPostById = async (id) => {
+  return await fetch(`http://localhost:9999/posts/${id}`).then((res) =>
+    res.json()
+  )
+}
+export const updatePost = (post) => {
+  return fetch(`http://localhost:9999/posts/${post.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ post }),
+  }).then((res) => res.json())
 }
